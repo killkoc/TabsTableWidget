8a1bc53c74d3ba94c644f4566c17bb40cded1abd
Killko <12544786+killkoc@users.noreply.github.com>
2024-10-21 20:04:57 +0200

    Update ttmWebFunctions.js
    Signed-off-by: Killko <12544786+killkoc@users.noreply.github.com>

--- mini/ttmWebFunctions.js
+++ mini/ttmWebFunctions.js
@@ -50,7 +50,9 @@ function ttmSwitchToLanguage(language) {
         pathParts.unshift(language);
     }
     const newPath = '/' + pathParts.join('/');
-    if (newPath !== window.location.pathname) window.location.pathname = newPath; // Only update if there's a change
+//    if (newPath !== window.location.pathname) window.location.pathname = newPath; // Only update if there's a change
+
+	return newPath;
 }
 
 function ttmSetLanguage(key, language) {
